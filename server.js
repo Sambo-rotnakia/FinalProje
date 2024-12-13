27e84812972b2a4f28fffec8b20b18a8328ca6d3
Sambo-rotnakia <rotnakia.sambo@student.cadt.edu.kh>
2024-12-13 14:53:13 +0700

    kk

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ import path from 'path';
 
 var app = express()
 
+app.get('/test', function(req,res){
+    res.sendFile(path.join(__dirname,'views/pages','index.html'));
+})
+
+app.listen(8080);
+console.log("Server is listening ")
